9b54ed7fdf1d02e8a92841f806b3dc718ec4b95a
HenryAprendo <hjsmipuc@gmail.com>
2023-09-06 13:08:26 -0500

    Insertando las imagenes de fondo para el home desde un archivo css y creando un nuevo componente para el navbar

---
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styles: [
+  ]
+})
+export class NavbarComponent {
+
+  isMenuActive = false;
+
+  toggleIsMenu(){
+    this.isMenuActive = !this.isMenuActive;
+  }
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../components/shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './home.component.html',
-  styles: []
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
-  isMenuActive = false;
-
-  toggleIsMenu(){
-    this.isMenuActive = !this.isMenuActive;
-  }
 
 }
